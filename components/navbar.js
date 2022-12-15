dc513a35d24f99dc046093708ec8eb5324e0de3a
Deepak1095 <ddhankhar999@gmail.com>
2022-12-15 22:47:09 +0530

    add footer and navabr

--- components/navbar.js
+++ components/navbar.js
@@ -108,4 +108,4 @@ const footer=()=>{
     `
 }
 
-export default {navbar,footer}
+export {navbar,footer}
--- scripts/fragrance.js
+++ scripts/fragrance.js
@@ -1,3 +1,11 @@
+import{navbar,footer} from "../components/navbar.js"
+
+let navbar_div=document.getElementById("navbar");
+navbar_div.innerHTML=navbar()
+
+let footer_div=document.getElementById("footer");
+footer_div.innerHTML=footer();
+
 let url="https://js211-project.onrender.com/fragrance"
 let product=async()=>{
     let res=await fetch(url)
