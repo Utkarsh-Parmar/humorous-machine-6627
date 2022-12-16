be186fa50b8dab04f4ca1fd765e89857784b26a3
Deepak1095 <ddhankhar999@gmail.com>
2022-12-16 06:03:29 +0530

    details and cart page

---
+++ scripts/details.js
@@ -0,0 +1,68 @@
+import{navbar,footer} from "../components/navbar.js"
+
+let navbar_div=document.getElementById("navbar");
+navbar_div.innerHTML=navbar()
+
+let footer_div=document.getElementById("footer");
+footer_div.innerHTML=footer();
+let id=localStorage.Data_id
+let url="https://js211-project.onrender.com/skin"
+let product=async(pageNo=1)=>{
+    let res=await fetch(`${url}/${id}`)
+   res= await res.json()
+  renderDom([res])
+  console.log(res)
+}
+product()
+let renderDom=(data)=>{
+    let cont=document.getElementById("container")
+      cont.innerHTML=null
+      data.forEach((el)=>{
+         
+          let div1=document.createElement("div")
+          div1.className="main_image"
+          let div2=document.createElement("div")
+          let image=document.createElement("img")
+          image.src=el.image
+          let name=document.createElement("h5")
+          name.innerText=el.name
+          name.className="Product_Name"
+          let starImage=document.createElement("img")
+          starImage.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNi46qN56UzUWRidUVf3g6vXp9pOscW5_mjw&usqp=CAU"
+          starImage.className="starImages"
+          let brand=document.createElement("p")
+          brand.innerText="BRAND  "+el.brand
+          brand.className="Product_Brand"
+          let amount=document.createElement("p")
+          amount.innerText=el.amount
+          amount.className="Product_amount"
+          let price=document.createElement("h5")
+          price.innerText="Rs "+(+el.price)
+          price.className="Product_Price"
+          div1.append(image)
+          let addToCart=document.createElement("button")
+        addToCart.className="add_to_cart"
+        addToCart.innerHTML=`<i class="fa-solid fa-bag-shopping">Add To Cart</i>`
+        addToCart.onclick=()=>{
+        
+           window.location.href="cart.html"
+           
+          let cartData=JSON.parse(localStorage.getItem("cart_data"))||[]
+          console.log("1")
+          let obj={
+            id:el.id,
+            name:el.name,
+            brand:el.brand,
+            image:el.image,
+            price:el.price,
+            amount:el.amount
+          }
+          cartData.push(obj)
+   localStorage.setItem("cart_data", JSON.stringify(cartData));
+            }
+          div2.append(name,amount,starImage,brand,price,addToCart)
+          cont.append(div1,div2)
+      })
+  }
+
+delete localStorage.Data_id;
--- scripts/skin.js
+++ scripts/skin.js
@@ -10,9 +10,7 @@ footer_div.innerHTML=footer();
 
 let url="https://js211-project.onrender.com/skin"
 let product=async(pageNo=1)=>{
-     let res=await fetch(`${url}?_limit=10&_page=${pageNo}`)
-  //   https://js211-project.onrender.com/skin?_limit=4&_page=2
-    // let res=await fetch(`url?_limit=4_page=${pageNo}`)
+     let res=await fetch(`${url}?_limit=9&_page=${pageNo}`)
     res= await res.json()
    renderDom(res)
    console.log(res)
@@ -22,7 +20,86 @@ let renderDom=(data)=>{
     let cont=document.getElementById("container_skin")
     cont.innerHTML=null
     data.forEach((el)=>{
+
+        let div=document.createElement("div")
+        div.onclick=()=>{
+          window.location.href="details.html"
+          localStorage.Data_id=el.id
+        }
+        let image=document.createElement("img")
+        image.src=el.image
+        image.onclick=()=>{
+          console.log("2")
+        }
+        let name=document.createElement("h5")
+        name.innerText=el.name
+        name.className="Product_Name"
+        let starImage=document.createElement("img")
+        starImage.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNi46qN56UzUWRidUVf3g6vXp9pOscW5_mjw&usqp=CAU"
+        let price=document.createElement("h5")
+        price.innerText=+el.price
+        price.className="Product_Price"
+        let addToCart=document.createElement("button")
+        addToCart.className="add_to_cart"
+        addToCart.innerHTML=`<i class="fa-solid fa-bag-shopping">Add To Cart</i>`
+        div.append(image,name,starImage,price,addToCart)
         
+        cont.append(div)
+    })
+}
+ let pageNo=1
+
+let sortlh=document.getElementById("sortlh").onclick=()=>{
+  low(1)
+}
+let low=async(pageNo)=>{
+let res=await fetch(`${url}?_limit=9&_page=${pageNo}&_sort=price&_order=asc`)
+ let data= await res.json()
+renderDom(data)
+console.log(data)
+}
+
+let sorthl=document.getElementById("sorthl").onclick=()=>{
+  high(1)
+}
+let high=async(pageNo)=>{
+  let res=await fetch(`${url}?_limit=9&_page=${pageNo}&_sort=price&_order=desc`)
+  let  data= await res.json()
+  renderDom(data)
+  console.log(data)
+   
+  }
+
+let showButton=()=>{
+  let btn=document.getElementById("btn")
+  btn.innerHTML=null
+  
+  for(let i=1;i<6;i++){
+    let btns=document.createElement("button")
+    btns.innerText=i
+    btns.onclick=()=>{
+      product(i)
+      document.getElementById("sortlh").onclick=()=>{
+        low(i)
+      }
+      document.getElementById("sorthl").onclick=()=>{
+        high(i)
+      }
+    }
+    
+    btn.append(btns)
+ 
+  }
+}
+console.log(pageNo)
+showButton()
+
+
+let renderDom1=(data,brand_Name)=>{
+  let cont=document.getElementById("container_skin")
+    cont.innerHTML=null
+    data.forEach((el)=>{
+        if(el.brand==brand_Name){
         let div=document.createElement("div")
         let image=document.createElement("img")
         image.src=el.image
@@ -35,57 +112,17 @@ let renderDom=(data)=>{
         price.innerText=+el.price
         price.className="Product_Price"
         div.append(image,name,starImage,price)
-        cont.append(div)
+        cont.append(div)}
     })
 }
- let pageNo=1
-  let showButton=()=>{
-    let btn=document.getElementById("btn")
-    btn.innerHTML=null
-    
-    for(let i=1;i<4;i++){
-      let btns=document.createElement("button")
-      btns.innerText=i
-      btns.onclick=()=>{
-        product(i)
-      pageNo=i
-        showButton()
-      }
-      
-      btn.append(btns)
-   
-    }
-  }
-  console.log(pageNo)
-showButton()
 
+document.getElementById("select_Brand").onchange=(event)=>{
+let product1=async()=>{
+  let res=await fetch(url)
+ res= await res.json()
 
-
-let low=async(pageNo)=>{
-// let res=await fetch(`${url}?_limit=4&_page=${pageNo}?_sort=price&_order=asc`)
-//   data= await res.json()
-// renderDom(data)
-// console.log(data)
- console.log(pageNo)
+  let brand_Name=event.target.value
+  renderDom1(res,brand_Name)
 }
-let high=async(pageNo)=>{
-  let res=await fetch(`${url}?_limit=4&_page=${pageNo}&_sort=price&_order=desc`)
-    data= await res.json()
-  renderDom(data)
-  console.log(data)
-   
-  }
-
-
-
-let func=(i,event)=>{
-if(event.target.value=="high"){
-high(i)
-console.log(event.target.value)
-}else if(event.target.value=="low"){
-low(i)
-console.log(event.target.value)
+product1()
 }
-
-}
-let sorting=document.getElementById("priceSorting").addEventListener("change",func)
