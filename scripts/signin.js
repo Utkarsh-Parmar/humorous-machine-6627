383f2a424a0b0c85014421b623fae4c2d7d4c51a
Utkarsh420 <utkarshparmar344@gmail.com>
2022-12-17 22:13:50 +0530

    Working signin and sign up

--- scripts/signin.js
+++ scripts/signin.js
@@ -7,59 +7,12 @@ let footer_div=document.getElementById("footer");
 footer_div.innerHTML=footer();
 
 let data = JSON.parse(localStorage.getItem("signinData")) || [];
+// console.log(data[1].user_email);
 
-let sign_up_btn = document.getElementById("sign-up-btn");
+var user_sign_in = document.getElementById("new");
+user_sign_in.addEventListener("click", newSign);
+function newSign(){
 
-function getFormData(){
-    let form = document.getElementById("user_sign_up_form")
-    let user_email = document.getElementById("user-email").value;
-    let user_fname = document.getElementById("user-first-name").value;
-    let user_lname = document.getElementById("user-last-name").value;
-    let user_number = document.getElementById("user-number").value;
-    let user_password = document.getElementById("user-password").value;
-    let confirm_password = document.getElementById("confirm-password").value;
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-    if(user_email.match(validRegex) && user_password.length>7 && user_password ==confirm_password){
-        user_email = user_email;
-        addData(user_email,user_password,user_fname,user_lname,user_number,confirm_password);
-        form.reset();
-        window.location.replace("signin.html");
-    }else{
-        alert("Invalid email or Password is less than 8 characters");
-    }
-
-    function addData(user_email,user_password,user_fname,user_lname,user_number,confirm_password){
-        let p = {
-            user_email,
-            user_password,
-            user_fname,
-            user_lname,
-            user_number,
-            confirm_password
-        }
-        // data = JSON.parse(localStorage.getItem("signinData")) || [];
-        data.push(p);
-        localStorage.setItem("signinData",JSON.stringify(data));
-        alert("Sign Up successfully");
-    }
-
-}
-
-sign_up_btn.addEventListener("click",getFormData);
-
-
-var user_sign_in = document.getElementById("user_signin");
-user_sign_in.onclick = (event)=>{
-    signIn(event);
-}
-// user_sign_in.onclick=()=>{
-
-// signIn(event)
-// }
-
-function signIn(event){
-     event.preventDefault();
 
     console.log("I m clicked");
     
@@ -70,6 +23,10 @@ function signIn(event){
     for(let i=0;i<n;i++){
         if(data[i].user_email === email){
             if(data[i].user_password === signin_pass){
+                let usr_name = document.getElementById("btn3")
+                // usr_name.innerText = data[i].user_fname ;
+                usr_name.innerText = "Utkarsh" ;
+
                 window.location.replace("home.html");
                 return;
             }else{
@@ -84,3 +41,9 @@ function signIn(event){
         alert("Not a User! Please Create an Accout");
     }
 }
+
+
+
+
+// console.log()
+
