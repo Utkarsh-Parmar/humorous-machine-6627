4f763a052bfb6d099ce4d725280a3c6aa0c37e70
Deepak1095 <ddhankhar999@gmail.com>
2022-12-18 20:19:48 +0530

    link files

--- scripts/signin.js
+++ scripts/signin.js
@@ -5,6 +5,52 @@ navbar_div.innerHTML=navbar()
 
 let footer_div=document.getElementById("footer");
 footer_div.innerHTML=footer();
+//indexpage btn link
+let index_btn=document.getElementById('logo');
+index_btn.onclick=()=>{
+    window.location.href="index.html"
+}
+
+//register btn link
+let reg_btn=document.getElementById('register_btn');
+reg_btn.onclick=()=>{
+    window.location.href="signup.html"
+}
+//signin page
+let sign_in=document.getElementById("login_page");
+sign_in.onclick=()=>{
+    window.location.href="signin.html"
+}
+//admin login
+let admin_log=document.getElementById("admin_login")
+admin_log.onclick=()=>{
+    window.location.href="admin.html"
+}
+// linking the cart page
+let cart_btn=document.getElementById("my_cart_btn");
+cart_btn.onclick=()=>{
+    window.location.href="cart.html"
+}
+let fragrnace_btn=document.getElementById("fragrance_btn");
+fragrnace_btn.onclick=()=>{
+    window.location.href="fragrance.html"
+}
+
+//hair_btn
+let hair_btn=document.getElementById("hair_btn");
+hair_btn.onclick=()=>{
+    window.location.href="hair.html"
+}
+//skin page;
+let skin_page=document.getElementById("skin-btn")
+skin_page.onclick=()=>{
+    window.location.href="skin.html"
+}
+//fragrance page;
+let frag_page=document.getElementById("fragrance_btn")
+frag_page.onclick=()=>{
+    window.location.href="fragrance.html"
+}
 
 let data = JSON.parse(localStorage.getItem("signinData")) || [];
 // console.log(data[1].user_email);
